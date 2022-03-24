df90f1a6a5ef88e1871cfe87756b111bc3376077
Parker Rudd <parkerbrudd@gmail.com>
2022-03-24 14:19:21 -0600

    created react app and added search bar and bg images

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'; 
+const api = {
+  key: '81f0db3489f2d58ca49d1b97febd0332', 
+  base: 'https://api.openweathermap.org/data/2.5'
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <main>
+        <div className='search-box'>
+          <input 
+            type="text"
+            className='search-bar'
+            placeholder='Search...'/>
+        </div>
+      </main>
     </div>
   );
 }
